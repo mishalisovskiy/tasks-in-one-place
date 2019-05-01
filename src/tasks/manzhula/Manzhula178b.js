2d15ca32dd0096761fd68f6d81655f95940ff2ce
Manzhula David <da411d@gmail.com>
2019-05-01 23:32:28 +0300

    Fixes for Linter

--- src/tasks/manzhula/Manzhula178b.js
+++ src/tasks/manzhula/Manzhula178b.js
@@ -1,12 +1,7 @@
 import readlineSync from 'readline-sync';
 
-export const resolveTask178b = () => {
-  console.log('Find count of items numbers from 1 to n, which are multiple 3 and not multiple 5');
-  const n = Number(readlineSync.question('Please choose n: '));
-  console.log(findCount(n));
-};
 export const findCount = (n) => {
-  if (!n || isNaN(n)) return 'Invalid input, enter a number';
+  if (!n || Number.isNaN(n)) return 'Invalid input, enter a number';
   let count = 0;
   for (let i = 3; i <= n; i += 3) {
     if (i % 5) {
@@ -14,4 +9,10 @@ export const findCount = (n) => {
     }
   }
   return count;
+};
+
+export const resolveTask178b = () => {
+  console.log('Find count of items numbers from 1 to n, which are multiple 3 and not multiple 5');
+  const n = Number(readlineSync.question('Please choose n: '));
+  console.log(findCount(n));
 };
--- src/tasks/manzhula/Manzhula178v.js
+++ src/tasks/manzhula/Manzhula178v.js
@@ -1,16 +1,16 @@
 import readlineSync from 'readline-sync';
 
-export const resolveTask178v = () => {
-  console.log('Find count of items numbers from 1 to n, which are squares of even numbers.');
-  const n = Number(readlineSync.question('Please choose n: '));
-  console.log(findCount(n));
-};
-
 export const findCount = (n) => {
-  if (!n || isNaN(n)) return 'Invalid input, enter a number';
+  if (!n || Number.isNaN(n)) return 'Invalid input, enter a number';
   let count = 0;
   for (let i = 2; i * i <= n; i += 2) {
     count += 1;
   }
   return count;
+};
+
+export const resolveTask178v = () => {
+  console.log('Find count of items numbers from 1 to n, which are squares of even numbers.');
+  const n = Number(readlineSync.question('Please choose n: '));
+  console.log(findCount(n));
 };
