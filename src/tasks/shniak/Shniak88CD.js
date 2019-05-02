91f04b231608c63dff3aeaa8f4021076ee4a6b1a
Nazar Shniak <n.shniak@gmail.com>
2019-05-02 04:59:56 +0300

    Refactor my code to make it more easily to test; some changes to Oksana's functions to make them work with tests; add final tests

--- src/tasks/shniak/Shniak88CD.js
+++ src/tasks/shniak/Shniak88CD.js
@@ -1,27 +1,13 @@
-import readlineSync from 'readline-sync';
-
-export const swap = (n) => {
-  const number = n.toString();
-  const arr = Array.from(number);
-
-  const a = arr[0];
-  const b = arr[arr.length - 1];
-
-  arr[0] = b;
-  arr[arr.length - 1] = a;
-
-  arr.push(1);
-  arr.unshift(1);
-
-  const result = arr.join(''); // -- should've add '+' to return int, but it returns 2 digits numbers devided by ten without a leading zero
-  return result;
-};
+import { swap, whatNumber } from './utils';
 
 export const resolveTask88CD = () => {
   console.log("------------------------\nTask 88 (c and d) - Swap first and last element of a number (n) and add '1' on the beginning and the end.\n------------------------");
-  const n = Number(readlineSync.question('Please enter n: '));
+
+  const n = whatNumber();
 
   console.log(swap(n));
+
+  return swap(n);
 };
 
 export default resolveTask88CD;
