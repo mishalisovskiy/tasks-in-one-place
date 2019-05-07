ee7ecc47a9d7befcd683deb1f6d94fbe5d49951a
Oksana Rudka <rudka.oksana@gmail.com>
2019-05-07 23:34:59 +0300

    added unit test to Vitaliy's tasks

---
+++ src/tasks/tkachuk/Tkachuk331a.test.js
@@ -0,0 +1,31 @@
+import { findThree } from './Tkachuk331a';
+
+describe('Function findThree should return correct values', () => {
+  test('-1', () => {
+    expect(findThree(-1)).toEqual(false);
+  });
+  // test('0', () => {
+  //   expect(findThree(0)).toEqual(false); //'Invalid input, enter a number'
+  // });
+  test('1', () => {
+    expect(findThree(1)).toEqual(false);
+  });
+  test('3', () => {
+    expect(findThree(3)).toEqual(true);
+  });
+  test('13', () => {
+    expect(findThree(13)).toEqual(false);
+  });
+  test('14', () => {
+    expect(findThree(14)).toEqual(true);
+  });
+  test('null', () => {
+    expect(findThree(null)).toEqual('Invalid input, enter a number');
+  });
+  test('undefind', () => {
+    expect(findThree()).toEqual('Invalid input, enter a number');
+  });
+  test('string', () => {
+    expect(findThree('')).toEqual('Invalid input, enter a number');
+  });
+});
